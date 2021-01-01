1867e17cf60bc3c1fc44cc706d527364654aa8e3
AakankshaDC <aakankshadc32@gmail.com>
2021-01-01 14:12:26 -0500

    addede NEAT config file and incorporated thee algorithm to spawn multiple birds running their own neural nets and fitness function

--- flappy-bird.py
+++ flappy-bird.py
@@ -196,7 +196,7 @@ class Base:
 		window.blit(self.IMG, (self.x1, self.y))
 		window.blit(self.IMG, (self.x2 , self.y))
 
-def draw_window(window, bird, pipes, base, score):
+def draw_window(window, birdObjects, pipes, base, score):
 	# draw the background
 	window.blit(BG_IMAGE, (0,0)) # draw on top left
 	for pipe in pipes:
@@ -207,12 +207,26 @@ def draw_window(window, bird, pipes, base, score):
 	window.blit(text, (WINDOW_WIDTH - 10 - text.get_width(), 10))
 
 	base.draw(window)
+	for bird in birdObjects:
+		bird.draw(window)
 
-	bird.draw(window)
 	pygame.display.update()
 
-def main():
-	birdObject = Bird(230,350)
+def main(genomes, config):
+	birdObjects = [] # upadte this to deal with multiple birds
+	neural_nets = [] # neeural network for each bird
+	genome_list = [] # genome list to maintain fitness info for each bird
+
+	for _ , g in genomes:
+		net = neat.nn.FeedForwardNetwork.create(g, config)
+		neural_nets.append(net)
+		birdObjects.append(Bird(230,350))
+
+		g.fitness = 0 # set initial fitness of birds to be zero 
+		genome_list.append(g)
+
+	
+	
 	base = Base(730) # where to have the base photo should be
 	pipes = [Pipe(600)]
 	window =pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -226,40 +240,93 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
+				pygame.quit()
+				quit()
+
+		# move the brids based on the neural network
+		pipe_ind = 0
+		if len(birdObjects) > 0:
+			if len(pipes) > 1 and birdObjects[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width(): # if pipee 0 has passed
+				pipe_ind = 1
+		else:
+			# quit running the game
+			run = False
+			break
+
+		for x, bird in enumerate(birdObjects):
+			bird.move()
+			# give reward for coming this far
+			genome_list[x].fitness += 0.1
+
+			# activate our neural network
+			output = neural_nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
+			if output[0] > 0.5:
+				bird.jump()
 
 		# birdObject.move() # to make our bird move but will immediately fall, thus a tick rate
 		add_pipe = False
 		rem = []
 		for pipe in pipes:
-			# collision detection 
-			if pipe.collide(birdObject):
-				pass
+			for x, bird in enumerate(birdObjects):
+				# collision detection 
+				if pipe.collide(bird):
+					# remove birds that collide and stagnate its fitness
+					genome_list[x].fitness -= 1
+					# remove all
+					birdObjects.pop(x)
+					neural_nets.pop(x)
+					genome_list.pop(x)
+
+				if not pipe.passed and pipe.x < bird.x: # as soon as the pipe is passed generate a new one
+					pipe.passed = True
+					add_pipe =True
 
 			if pipe.x + pipe.PIPE_TOP.get_width() < 0: # if pipe is completely off the screen, remove that pipe
 				rem.append(pipe)
 
-			if not pipe.passed and pipe.x < birdObject.x: # as soon as the pipe is passed generate a new one
-				pipe.passed = True
-				add_pipe =True
-
 			pipe.move()
 
+		# if a bird made it through a pipe
 		if add_pipe:
 			score += 1
+			# also add some fitness to the ones that passed the pipe 
+			for g in genome_list:
+				g.fitness += 5
 			# create a new pipe
 			pipes.append(Pipe(600))
 
 		for r in rem: # remove all the pipes that are off the screen
 			pipes.remove(r)
 
-		# check if the bird has hit the ground, this part is essential because we are going to use a genetic algorithm to identify which instance was a failure anyway
-		if birdObject.y + birdObject.img.get_height() >= 730: # we hit the floor
-			pass
+		for x, bird in enumerate(birdObjects):
+			# check if the bird has hit the ground, this part is essential because we are going to use a genetic algorithm to identify which instance was a failure anyway
+			if bird.y + bird.img.get_height() >= 730 or bird.y < 0: # we hit the floor or birds flew on the top of the screen
+				birdObjects.pop(x)
+				neural_nets.pop(x)
+				genome_list.pop(x)
 
 		base.move()
-		draw_window(window, birdObject, pipes, base, score)
+		draw_window(window, birdObjects, pipes, base, score)
 
-	pygame.quit()
-	quit()
+	# pygame.quit()
+	# quit()
 
-main()
+
+def run(config_file):
+	# load file here, then run the NEAT Algorithm
+	config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
+	
+	# use the config file here
+	population = neat.Population(config)
+	population.add_reporter(neat.StdOutReporter(True)) # stats of each generation to bee displayed in the terminal
+	stats = neat.StatisticsReporter()
+	population.add_reporter(stats)
+
+	winner = population.run(main, 50) # run fitness funtion for 50 generations, call main() 50 times and pass the geenome and config file
+
+if __name__ == "__main__":
+	# get path to config file
+	local_dir = os.path.dirname(__file__)
+	config_path = os.path.join(local_dir, "config-feedforward.txt")
+	run(config_path)
